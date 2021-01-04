24ccc14789825310ebd6af07c43c385f84eb6889
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-01-04 19:32:01 +0530

    localstorage added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    getLocalTodos();
+  },[])
+
+  useEffect(() => {
+    saveLocalTodos();
+  },[todos])
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos",JSON.stringify([]))
+    }else {
+      let todoLocal=JSON.parse(localStorage.getItem("todos"))
+      setTodos(todoLocal);
+    }
+  }
 
   return (
     <div className="App">
